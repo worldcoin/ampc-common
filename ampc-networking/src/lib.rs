562725d2781484bafdbe452610036e7399110ec5
Ertugrul Aypek <ertugrul.aypek@toolsforhumanity.com>
2025-11-12 13:00:22 +0100

    migrate as is

---
+++ ampc-networking/src/lib.rs
@@ -0,0 +1,42 @@
+use async_trait::async_trait;
+use iris_mpc_cpu::execution::player::Identity;
+use eyre::Result;
+use crate::value::NetworkValue;
+
+pub mod local;
+pub mod tcp;
+pub mod value;
+
+/// Requirements for networking.
+#[async_trait]
+pub trait Networking {
+    async fn send(&mut self, value: NetworkValue, receiver: &Identity) -> Result<()>;
+
+    async fn receive(&mut self, sender: &Identity) -> Result<NetworkValue>;
+}
+
+#[derive(Clone)]
+pub enum NetworkType {
+    Local,
+    Tcp {
+        connection_parallelism: usize,
+        request_parallelism: usize,
+    },
+}
+
+impl NetworkType {
+    pub fn default_connection_parallelism() -> usize {
+        1
+    }
+
+    pub fn default_request_parallelism() -> usize {
+        1
+    }
+
+    pub fn default_tcp() -> Self {
+        Self::Tcp {
+            connection_parallelism: Self::default_connection_parallelism(),
+            request_parallelism: Self::default_request_parallelism(),
+        }
+    }
+}
