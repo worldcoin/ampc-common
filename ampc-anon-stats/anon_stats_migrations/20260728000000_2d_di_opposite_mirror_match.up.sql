5f5ee596086ba6a57faae19287684e248fb0b134
Elena Gryaznova <elena.gryaznova@toolsforhumanity.com>
2026-07-28 12:53:57 +0200

    Make false default value

--- ampc-anon-stats/anon_stats_migrations/20260728000000_2d_di_opposite_mirror_match.up.sql
+++ ampc-anon-stats/anon_stats_migrations/20260728000000_2d_di_opposite_mirror_match.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE anon_stats_2d_di
-    ADD COLUMN IF NOT EXISTS left_opposite_mirror_match BOOLEAN;
+    ADD COLUMN IF NOT EXISTS left_opposite_mirror_match BOOLEAN DEFAULT FALSE;
 
 ALTER TABLE anon_stats_2d_di
-    ADD COLUMN IF NOT EXISTS right_opposite_mirror_match BOOLEAN;
+    ADD COLUMN IF NOT EXISTS right_opposite_mirror_match BOOLEAN DEFAULT FALSE;
