5366f6641e77231b58874c2708423665915416b5
Elena Gryaznova <elena.gryaznova@toolsforhumanity.com>
2026-08-28 16:47:01 +0200

    Add default value

--- ampc-anon-stats/src/server/config/mod.rs
+++ ampc-anon-stats/src/server/config/mod.rs
@@ -379,6 +379,7 @@ impl AnonStatsServerConfig {
                 default_shutdown_last_results_sync_timeout_secs(),
             tls: None,
             min_2d_job_size_opposite_mirror_match: default_min_2d_job_size_opposite_mirror_match(),
+            ignore_missing_migrations: false,
         }
     }
 
